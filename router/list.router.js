fd283dd48d0e7d68fdfbd71b8d8932ebc670f382
SantGT5 <gianspf@gmail.com>
2021-09-12 22:37:33 +0200

    GianLucas app Cors

--- router/list.router.js
+++ router/list.router.js
@@ -8,7 +8,8 @@ router.get("/tickets", async (req, res) => {
       "https://6f7smj4fdc.execute-api.us-east-1.amazonaws.com/default/techJobMission"
     );
 
-    return res.status(200).json(response.data);
+    return res.status( 200 ).json( response.data );
+
   } catch (err) {
     console.log(err.response);
     return res.status( 404 ).json({ msg: "List not found" })
